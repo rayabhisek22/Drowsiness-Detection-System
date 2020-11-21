5dfe0edd2fb083a5333a3af7ae04236302847d63
rayabhisek22 <rayabhisek22@gmail.com>
2020-11-21 19:05:25 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,9 @@
+# drowsinessDetector
+OpenCV Drowsiness Detector using Python and Dlib
+
+Packages to install
+opencv-python
+cmake
+dlib
+scipy
+playground
---
+++ drowsinessDetector.py
@@ -0,0 +1,87 @@
+import cv2
+import dlib
+from scipy.spatial import distance
+from playsound import playsound
+
+
+def calculate_EAR(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear_aspect_ratio = (A + B) / (2.0 * C)
+    return ear_aspect_ratio
+
+
+cap = cv2.VideoCapture(0)
+
+hog_face_detector = dlib.get_frontal_face_detector()
+dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+count =0
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = hog_face_detector(gray)
+    for face in faces:
+
+        face_landmarks = dlib_facelandmark(gray, face)
+        leftEye = []
+        rightEye = []
+
+        a = face_landmarks.part(0).x
+        b = face_landmarks.part(19).y-70
+        w = face_landmarks.part(16).x
+        h = face_landmarks.part(9).y
+        cv2.rectangle(frame, (a, b), (w,h), (0, 255, 0), 2)
+
+        for n in range(36, 42):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            leftEye.append((x, y))
+            next_point = n + 1
+            if n == 41:
+                next_point = 36
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame, (x, y), (x2, y2), (240, 0, 255), 1)
+
+        for n in range(42, 48):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            rightEye.append((x, y))
+            next_point = n + 1
+            if n == 47:
+                next_point = 42
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame, (x, y), (x2, y2), (240, 0, 255), 1)
+
+        left_ear = calculate_EAR(leftEye)
+        right_ear = calculate_EAR(rightEye)
+
+        EAR = (left_ear + right_ear) / 2
+        EAR = round(EAR, 2)
+        if EAR < 0.28:
+            count=count+1
+        else:
+            count=0
+
+        print(EAR)
+        if count > 20:
+            cv2.putText(frame, "DROWSY", (20, 100),
+                        cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 4)
+            print("Drowsy")
+            for i in range(1,3):
+                playsound('mysound.wav')
+            count=0
+
+
+    cv2.imshow("Are you Sleepy", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
